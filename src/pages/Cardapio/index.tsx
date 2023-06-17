5e962c10813e4e16fa02cb14efbfd5d2bf1b27df
Renan Igor <renanigor314@gmail.com>
2023-06-17 17:09:10 -0300

    Projeto finalizado.

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -6,8 +6,6 @@ import Ordenador from './Ordenador';
 import Itens from './Itens';
 import stylesTema from '../../styles/Tema.module.scss';
 
-
-
 export default function Cardapio() {
 
   const [busca, setBusca] = useState('');
